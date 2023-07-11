c15cdf74a6d5e48f4bd27f444e82abec593cd326
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-07-11 02:34:54 +0000

    fix "delete icon" display

--- src/js/component/Todo.js
+++ src/js/component/Todo.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { icon } from "@fortawesome/fontawesome-svg-core";
 
-const Todo = ({task, deleteTodo}) =>{
+const Todo = ({ task, deleteTodo }) => {
 
-    
-
-    return(
+    return (
         <div className="toDo">
             <p>{task.task}</p>
-            <div>
-            <FontAwesomeIcon className="icon" icon={faTrashCan} onClick={() => deleteTodo(task.id)}/>
+            <div className="buttonDiv">
+                <button className="iconButton">
+                    <FontAwesomeIcon className="icon" icon={faTrashCan} onClick={() => deleteTodo(task.id)} />
+                </button>
             </div>
         </div>
     )
--- src/js/component/TodoForm.js
+++ src/js/component/TodoForm.js
@@ -7,6 +7,7 @@ import { useState } from "react";
         
     const [userValue, setUserValue] = useState("")
 
+    
     // e is an event passed to the function to handle info submitted by user
     const handleSubmit = (e) =>{
         // function below prevents reloading when submitting
@@ -16,8 +17,12 @@ import { useState } from "react";
 
         // clear input space after submit
         setUserValue("")
+
+        
+
     }
     return(
+        
         <form className="todoForm text-center" onSubmit={handleSubmit}>
         <input className="todoInput" type="text" value={userValue} placeholder="Add a new task" onChange={(e) => setUserValue(e.target.value)}></input>
         <button className="todoButton" type="submit"> Add task </button>
